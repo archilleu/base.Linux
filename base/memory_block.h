cb1bf568a74593bc3b14ffaa67b79ba91c39b133
man <313720180@qq.com>
2016-03-16 11:27:38 +0800

    fix mb op[]

--- base/memory_block.h
+++ base/memory_block.h
@@ -22,7 +22,8 @@ public:
     char*   dat()   { return dat_; }
     size_t  len()   { return len_; }
 
-    char& operator[] (size_t index) { return dat_[index]; }
+    char    operator[] (size_t index) const { return dat_[index]; }
+    char&   operator[] (size_t index)       { return dat_[index]; }
 
 private:
     friend bool operator==  (const MemoryBlock& left, const MemoryBlock& right);
